c29ef607e0cdb74c53491e932b130647ad6ff1d8
Alexey Kugachev <alexey_kugachev@epam.com>
2018-07-06 18:02:07 +0300

    lj

--- Task02/Benefits/Apple.cs
+++ Task02/Benefits/Apple.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Task02.Benefits
 {
     class Apple : BaseBenefit
     {
+        public Apple(Point p) : base(p) { }
         public override void Draw()
         {
             throw new NotImplementedException();
